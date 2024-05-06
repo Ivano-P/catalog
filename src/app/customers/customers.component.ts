d04c06061c41e4afd56059d3104012609ebbc6d6
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2024-05-06 13:05:49 +0200

    imported bootstrap-icons.css

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css'
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit {
+  constructor() {
+  }
 
+  ngOnInit() {
+  }
 }
